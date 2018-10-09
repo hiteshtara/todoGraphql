94657cd815e0350c142617852b7dcb39f22d34c9
mukadder <mukadder@bu.edu>
2018-10-09 09:50:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const dinnerOptions = ['🍕', '🌭', '🍔', '🥗', '🍣'];
+
+const typeDefs = `
+  type Query {
+    whatsForDinner: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    whatsForDinner: () => {
+      const idx = Math.floor(Math.random() * dinnerOptions.length);
+      const foodChoice = dinnerOptions[idx];
+      return `Tonight we eat ${foodChoice}`;
+    },
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => console.log('Server is running on localhost:4000'));
