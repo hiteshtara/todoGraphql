948cb2da91f7f1c9f2db48afceed26be34ea3f7a
mukadder <mukadder@bu.edu>
2018-10-09 10:27:45 -0400

    exercize2 complete

---
+++ Readme.md
@@ -0,0 +1,18 @@
+query {
+whatsForDinner
+}
+
+-- run this query
+You can try it our and play around the API using GraphQL playground. For example, to delete the todo with ID 0:
+mutation {
+deleteTodo(id: "0") {
+id
+content
+}
+}
+mutation {
+createTodo(content: "Take a little nap 🛌", isCompleted: false) {
+content
+isCompleted
+}
+}
---
+++ index2.js
@@ -0,0 +1,90 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+let count = 2;
+let todos = [
+  {
+    id: '0',
+    content: 'Buy Milk',
+    isCompleted: true,
+  },
+  {
+    id: '1',
+    content: 'cook some lobster',
+    isCompleted: false,
+  },
+];
+const typeDefs = `
+type Todo {
+  id: ID!
+  content: String!
+  isCompleted: Boolean!
+}
+type Query {
+  allTodos: [Todo!]!
+  Todo(id: ID!): Todo!
+}
+type Mutation {
+  createTodo(content: String!, isCompleted: Boolean!): Todo!
+  updateTodo(id: ID!, content: String, isCompleted: Boolean): Todo!
+  deleteTodo(id: ID!): Todo!
+}
+`;
+
+const resolvers = {
+  Query: {
+    allTodos: () => {
+      return todos;
+    },
+    Todo: (_, { id }) => {
+      const todo = todos.find(x => x.id === id);
+      if (!todo) {
+        throw new Error('Cannot find your todo!');
+      }
+      return todo;
+    },
+  },
+  Mutation: {
+    createTodo: (_, { content, isCompleted }) => {
+      const newTodo = {
+        id: count++,
+        content,
+        isCompleted,
+      };
+      todos = [...todos, newTodo];
+      return newTodo;
+    },
+    updateTodo: (_, { id, content, isCompleted }) => {
+      let updatedTodo;
+
+      todos = todos.map(todo => {
+        if (todo.id === id) {
+          updatedTodo = {
+            id: todo.id,
+            // for content and isCompleted, we first check if values are provided
+            content: content !== undefined ? content : todo.content,
+            isCompleted:
+              isCompleted !== undefined ? isCompleted : todo.isCompleted,
+          };
+          return updatedTodo;
+        } else {
+          return todo;
+        }
+      });
+
+      return updatedTodo;
+    },
+    deleteTodo: (_, { id }) => {
+      const todoToDelete = todos.find(x => x.id === id);
+
+      todos = todos.filter(todo => {
+        return todo.id !== todoToDelete.id;
+      });
+
+      return todoToDelete;
+    },
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => console.log('Server is running on localhost:4000'));
